9e48d8d4f058fbccb775e2575f83d63113a47ac6
korko <korkos@list.ru>
2022-08-21 14:47:35 +0300

    дз 3-3

--- src/Main.java
+++ src/Main.java
@@ -2,4 +2,20 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
-}
+
+    public static void task3 () {
+        int year = 2021;
+        if (year % 4 = 0 && year % 100 != 0 || year % 400 = 0 ) {
+
+            System.out.println(year + " является високосным" );
+        } else {
+            System.out.println(year + " не  является високосным" );
+
+
+        }
+    }
+    public static void main(String[] args) {
+
+
+
+    }
