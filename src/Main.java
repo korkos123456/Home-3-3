981fad996f274ff15c7d9fbf828e441f04a3dd9c
korko <korkos@list.ru>
2022-08-21 15:22:23 +0300

    дз 3-3

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,58 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
 
-    public static void task3 () {
-        int year = 2021;
-        if (year % 4 = 0 && year % 100 != 0 || year % 400 = 0 ) {
 
-            System.out.println(year + " является високосным" );
-        } else {
-            System.out.println(year + " не  является високосным" );
+        public static void task4() {
+            int deliveryDistance = 95;
 
+            int deliveryDays = 1;
+
+            if (deliveryDistance > 20) {
+                deliveryDays++;
+            }
+            if (deliveryDistance > 60) {
+                deliveryDays++;
+                System.out.println("Потребуется дней:" + deliveryDays);
+            }
+        }
+        public static void main(String[] args) {
+
+            public static void task 5() {
+                int mothNumber =12;
+
+                switch (mothNumber) {
+                    case 12:
+                    case 1:
+                    case 2:
+                        System.out.println("Зима");
+                        break;
+                    case 3:
+                    case 4:
+                    case 5:
+                        System.out.println ("весна");
+                        break;
+                    case 6:
+                    case 7:
+                    case 8:
+                        System.out.println("Лето");
+                        break;
+                    case 9:
+                    case 10:
+                    case 11:
+                        System.out.println("Осень");
+                        break;
+                    default:
+                        System.out.println("Некорректный месяц"+ mothNumber);
+
+
+
+                }
+
+            }
+
+            }
+        }
 
         }
-    }
-    public static void main(String[] args) {
 
 
-
-    }
